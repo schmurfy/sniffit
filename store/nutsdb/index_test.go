dba904895e24d9a86d634cbcf07b0693bd90ef5d
Julien Ammous <schmurfy@gmail.com>
2022-08-10 18:12:13 +0200

    update

--- store/nutsdb/index_test.go
+++ store/nutsdb/index_test.go
@@ -1,24 +1,24 @@
 package nuts
 
-import (
-	"testing"
-	"time"
+// import (
+// 	"testing"
+// 	"time"
 
-	"github.com/franela/goblin"
+// 	"github.com/franela/goblin"
 
-	"github.com/schmurfy/sniffit/index_encoder"
-	"github.com/schmurfy/sniffit/store"
-)
+// 	"github.com/schmurfy/sniffit/index_encoder"
+// 	"github.com/schmurfy/sniffit/store"
+// )
 
-func TestNuts(t *testing.T) {
-	g := goblin.Goblin(t)
-	g.Describe("nutsdb", func() {
-		store.TestIndex(g, func(path string, encoder index_encoder.Interface) (store.StoreInterface, error) {
-			opts := NutsDefaultOptions
-			opts.Path = path
-			opts.Encoder = encoder
-			opts.TTL = 7 * 24 * time.Hour
-			return New(&opts)
-		})
-	})
-}
+// func TestNuts(t *testing.T) {
+// 	g := goblin.Goblin(t)
+// 	g.Describe("nutsdb", func() {
+// 		store.TestIndex(g, func(path string, encoder index_encoder.Interface) (store.StoreInterface, error) {
+// 			opts := NutsDefaultOptions
+// 			opts.Path = path
+// 			opts.Encoder = encoder
+// 			opts.TTL = 7 * 24 * time.Hour
+// 			return New(&opts)
+// 		})
+// 	})
+// }
