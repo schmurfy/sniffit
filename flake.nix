e2935ca6467a64c439acffd272b850acc72fd7b2
Julien Ammous <schmurfy@gmail.com>
2025-11-02 19:34:54 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "hetzner dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.clickhouse-lts
+          pkgs.protobuf_33
+          pkgs.libpcap
+        ];
+      };
+    };
+}
