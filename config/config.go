81e7a786888fd85ba8a7d5705d69bc25f7b510cd
Julien Ammous <schmurfy@gmail.com>
2025-12-15 12:01:34 +0100

    fix env config

--- config/config.go
+++ config/config.go
@@ -14,33 +14,33 @@ type Config struct {
 	ExportTracesToNewRelic bool   `config:"newrelic,description=Export traces to NewRelic"`
 	LightStep              bool   `config:"lightstep"`
 	UptraceDSN             string `config:"uptrace_dsn"`
-	SnapLen                int32  `config:"snap-len"`
+	SnapLen                int32  `config:"snap_len"`
 }
 
 type ArchivistConfig struct {
 	Config
 
-	ListenGRPCAddress string        `config:"listen-grpc,required,description=GRPC address to listen on"`
-	ListenHTTPAddress string        `config:"listen-http,required"`
-	DataPath          string        `config:"data-path"`
-	IndexPath         string        `config:"index-path"`
+	ListenGRPCAddress string        `config:"listen_grpc,required,description=GRPC address to listen on"`
+	ListenHTTPAddress string        `config:"listen_http,required"`
+	DataPath          string        `config:"data_path"`
+	IndexPath         string        `config:"index_path"`
 	DataRetention     time.Duration `config:"retention"`
-	StoreType         string        `config:"store-type,required"`
+	StoreType         string        `config:"store_type,required"`
 
 	// clickhouse
-	ClickhouseAddr     string `config:"clickhouse-addr"`
-	ClickhouseDatabase string `config:"clickhouse-database"`
-	ClickhouseUsername string `config:"clickhouse-username"`
-	ClickhousePassword string `config:"clickhouse-password"`
+	ClickhouseAddr     string `config:"clickhouse_addr"`
+	ClickhouseDatabase string `config:"clickhouse_database"`
+	ClickhouseUsername string `config:"clickhouse_username"`
+	ClickhousePassword string `config:"clickhouse_password"`
 }
 
 type AgentConfig struct {
 	Config
 
-	ArchivistAddress string `config:"archivist-address,required"`
+	ArchivistAddress string `config:"archivist_address,required"`
 	Filter           string `config:"filter,required,description=bpf filter used for capture"`
 	InterfaceName    string `config:"interface,required,description=interface to listen on"`
-	AgentName        string `config:"agent-name,required,description=the name is used to identify packet source in archivist"`
+	AgentName        string `config:"agent_name,required,description=the name is used to identify packet source in archivist"`
 }
 
 func Load(config any) error {
